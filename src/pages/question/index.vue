a2b8f7ad59310f2830dcccbb8d8dedb586844f52
lipeng <lipeng@daokoudai.com>
2019-01-24 18:42:43 +0800

    9.mpvue-课程定制问题页面搭建v0.2 页面模块搭建

--- src/pages/question/index.vue
+++ src/pages/question/index.vue
@@ -1,12 +1,27 @@
 <template lang='pug'>
-    .question 推荐课程测试    
+    .question
+        img.bg(src='/static/imgs/test.jpg')
+        .qs_content(v-if='questions.length > 0')
+            div(v-if='!startqs')
+                p.title 温馨提示
+                .warn_tag 为了更好对的为您制定学习计划，请首先回答以下问题，我们会为您量身推荐相关的课程
+            div(v-else)
+                p.title {{questions[currentIndex].title}}}
+                .response(v-for="(item,index) in questions[currentIndex].option" :key='index')
+                    img(v-if='item.select' src='/static/imgs/selected.jpg')
+                    img(v-else src='/static/imgs/unselect.jpg')
+                    span {{item.label}}
+        button.qs_btn {{btn_title}}
 </template>
 
 <script>
 export default {
     data(){
         return {
-            questions:[]
+            questions:[],
+            startqs: false, // 是否显示问题列表
+            currentIndex: 0, // 记录当前回答到第几道题
+            btn_title: '开始答题', // 按钮的title
         }
     },
     onLoad(){
@@ -26,3 +41,52 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.question {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  position: relative;
+}
+.question .bg {
+  width: 100%;
+  height: 100%;
+}
+.qs_content {
+  position: absolute;
+  width: 80%;
+  height: 50%;
+  background: #fff;
+  top: 36%;
+  left: 10%;
+  border-radius: 5px;
+  box-sizing: border-box;
+  padding: 10px;
+  color: rgb(13, 28, 51);
+}
+.qs_btn {
+  position: absolute;
+  top: 90%;
+  left: 10%;
+  width: 80%;
+  background-color: #009eef;
+  color: white;
+}
+.title {
+  font-weight: bold;
+  margin-bottom: 20px;
+  word-break: break-all;
+}
+.warn_tag {
+  padding: 40px 10px;
+}
+.response {
+  padding: 10px;
+}
+.response img {
+  width: 20px;
+  height: 20px;
+  margin-right: 10px;
+}
+</style>
