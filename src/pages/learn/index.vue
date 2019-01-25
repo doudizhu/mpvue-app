1824570f5ce86e18002e0b6612a1cd132c426b86
lipeng <lipeng@daokoudai.com>
2019-01-25 09:29:50 +0800

    12.mpvue-处理导航文字和学习顶部布局v0.2 学习顶部布局

--- src/pages/learn/index.vue
+++ src/pages/learn/index.vue
@@ -1,5 +1,14 @@
 <template lang='pug'>
-    div.learn 学习
+    scroll-view.learn(scroll-y)
+        .learn_info
+            img(src="/static/imgs/plan.jpg")
+            .info_text
+                h4 已学习
+                    span 0
+                    | 分钟
+                p 今日目标已完成
+                    span 0%
+
 </template>
 
 <script>
@@ -7,3 +16,90 @@ export default {
     
 }
 </script>
+
+<style scoped>
+.learn {
+  height: 100%;
+  box-sizing: border-box;
+}
+.learn_info {
+  padding: 10px;
+  display: flex;
+  flex-direction: row;
+  background-color: #fff;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  border: 1px solid #ebeef5;
+}
+.learn_info img {
+  width: 85px;
+  height: 85px;
+}
+.info_text {
+  padding: 10px;
+}
+.info_text h4 {
+  font-weight: bold;
+}
+.info_text h4 span {
+  color: #009eef;
+}
+.info_text p {
+  color: #ccc;
+  font-size: 14px;
+  margin-top: 10px;
+}
+.info_text p span {
+  color: #85c646;
+}
+
+.my_lesson .hot_lesson {
+  width: 100%;
+  margin-top: 16px;
+  background-color: #fff;
+  border: 1px solid #ebeef5;
+}
+
+.lesson_wrap {
+  width: 100%;
+  overflow-x: auto;
+}
+.lesson_scroll {
+  max-width: 200%;
+  min-width: 100%;
+  display: flex;
+  flex-direction: row;
+}
+.lesson_card {
+  display: flex;
+  flex-direction: column;
+  min-width: 120px;
+  padding: 10px 5px;
+}
+.lesson_card img {
+  width: 100%;
+  height: 100px;
+  border-radius: 5px;
+}
+.lesson_card span {
+  font-size: 14px;
+  color: #333;
+}
+::-webkit-scrollbar {
+  display: none;
+}
+.start_lesson {
+  padding: 20px 0;
+  box-sizing: border-box;
+  text-align: center;
+}
+.start_lesson button {
+  background-color: #fff;
+  color: #009eef;
+  width: 150px;
+  border-radius: 30px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+}
+.start_lesson button::after {
+  border: none;
+}
+</style>
