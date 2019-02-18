f1305c376bcd0d7481609444114af1b6658fd401
lipeng <lipeng@daokoudai.com>
2019-02-18 13:02:55 +0800

    23.mpvue-展示个人资料及组件封装

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,9 +1,81 @@
 <template lang='pug'>
-    div.mine 我的
+    .mine
+        .user_info
+            //- 用户信息
+            img(:src='user.avatarUrl')
+            .user_detail
+                h4 {{user.nickName}}
+                span {{user.gender == 1 ? '男' : '女'}}
+        .learn_info
+            //- 课程信息
+            .learn_detail
+                h4 课程
+                span {{lessonInfo.totalCount}}
+            .learn_detail
+                h4 已完成
+                span {{lessonInfo.finishCount}}
+            .learn_detail
+                h4 直播订阅
+                span {{lessonInfo.liveCount}}
 </template>
 
 <script>
 export default {
-    
+    computed: {
+        user(){
+            // console.log(this.$store.getters.user)
+            return this.$store.getters.user    
+        },
+        lessonInfo(){
+            // console.log(this.$store.getters.lessonInfo)
+            return this.$store.getters.lessonInfo    
+        },
+    }
 }
 </script>
+
+<style scoped>
+.user_info {
+  background-color: #fff;
+  padding: 8px 16px;
+  box-sizing: border-box;
+  border-bottom: 1px solid #ebeef5;
+  border-top: 1px solid #ebeef5;
+  display: flex;
+  flex-direction: row;
+}
+.user_info img {
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  margin-right: 20px;
+}
+.user_detail h4 {
+  margin: 10px 0;
+  font-weight: bold;
+}
+.user_detail span {
+  color: #aaa;
+  font-size: 14px;
+}
+.learn_info {
+  padding: 10px 16px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid #ebeef5;
+}
+.learn_info .learn_detail {
+  text-align: center;
+  flex: 1;
+}
+.learn_detail span {
+  color: #aaa;
+  font-size: 14px;
+}
+
+.order_info {
+  margin-top: 16px;
+}
+</style>
